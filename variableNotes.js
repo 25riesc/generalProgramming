c72aed6146044bbd46b1ce5a88b4fdebabb340da
Ries <25RiesC@badinhs.org>
2025-01-17 13:15:48 -0500

    variableNotes.js

--- variableNotes.js
+++ variableNotes.js
@@ -1,1 +1,46 @@
 //we will use a combination of comments and code to build up our notes
+//we start by
+// let myName = "jeff"
+// console.log(myName)
+// //here is a different way to declare a variable
+// const e = 2.71828
+// console.log(e)
+// //e = 3.141592
+// //console.log(e)
+// //^^ error :(
+// // a third way to create variable
+// var pi = 3.1415
+// console.log(pi)
+// pi = e
+// console.log(pi)
+// //var & let r interchangable, but const cant be changed
+// let total = 0
+// let i = 0
+// while (i < 1001){
+//     total = total + i
+//     i = i + 1
+// }
+//now we start with numbers and math
+//java can add
+//let sumOfSix = 1 + 3 + 6 + 10 + 21
+//console.log(sumOfSix)
+//you can store valued and do math in variables or in the console
+
+//here is a simulation of % error
+//const actual = 57
+//let prediction = actual-13
+//let percentage = prediction/actual
+//let percentError = 1-percentage
+//console.log (percent error)
+/* mathmatical operators in js
++ add
+- subtract
+* multiply
+/ divide
+** exponent
+% modulus
+
+//some rules for variables for js
+//name can only contain letters, digits, or $ or _
+//first character cannot be a digit
+//cannot be reserved words (no let, const, console.log)
