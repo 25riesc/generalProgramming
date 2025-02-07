df5e90bd3d73cb9ec8cc9ae7a7bdf36dfb3a771b
Ries <25RiesC@badinhs.org>
2025-02-07 11:02:51 -0500

    took notes

--- Functions.js
+++ Functions.js
@@ -11,24 +11,24 @@ function functionName(parameters){
 
 */
 //make a function that returns a message about a favorite animal
-function favoriteAnimal(animal){
-    return animal + " is my favorite animal"
-}
-console.log(favoriteAnimal("panda"))
+// function favoriteAnimal(animal){
+//     return animal + " is my favorite animal"
+// }
+// console.log(favoriteAnimal("panda"))
 
 //what happens inside a function is frequetly invisible to the outside world
 //referred to in programming as scope
 //local scope is in a function
 //global scope is main program
 //an example
-let test1 = 1
-let test2=5
-function add1(num) {//<-no reserved words
-    test2=test2+1
-    return num+1
-}
-console.log(add1(test1))
-console.log(test2)
+// let test1 = 1
+// let test2=5
+// function add1(num) {//<-no reserved words
+//     test2=test2+1
+//     return num+1
+// }
+// console.log(add1(test1))
+// console.log(test2)
 
 //to summarize- a function can change the value of a global variable 
 //but only if that variable is used by name withn the function
@@ -56,7 +56,50 @@ console.log(test2)
 // this.addEventListener("keydown", logKey)
 
 //arrow notation
-this.addEventListener("keydown", (event) => {
-    console.log(``)
+// this.addEventListener("keydown", (event) => {
+//     console.log(`you pressed ${event.key}`)
+// }
+// )
+//an example to determine scope
+// let x = 11
+// function a(){
+//     let y = 2
+//     output(y)
+// }
+// function b(){
+//     let z=3
+//     output(z)
+// }
+// function output(value){
+//     console.log(`value=${value}`);
+// }
+// output(x)
+// a()
+// b()
+
+//functions can have return values
+//thats soemthing that the function sends back to the global scope
+//some functions dont have a return value
+
+function randomLessThan(num) {
+    let x = Math.floor(Math.random()*num)
+    return x
 }
-)
+randomLessThan(4)
+
+//console.log(randomLessThan(5))
+//produce a # between 1 & 4
+console.log(randomLessThan(4) + 1)
+//if you want a function to interact with something else
+//you usually need a return value
+
+
+
+
+
+
+
+
+
+
+
