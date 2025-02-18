65bc4cc30986a0989ad95621d97abafe2985797a
Ries <25RiesC@badinhs.org>
2025-02-18 13:09:11 -0500

    finsihed loopnotes, starting array notes

--- loopsNotes.js
+++ loopsNotes.js
@@ -1,3 +1,4 @@
+
 /* notes on loops on js
 a great way to repeat a set of steps
 if you have something you want to do multiple times in a row
@@ -46,3 +47,56 @@ example */
 /* with either form, if your condition is based on a #, use <,>,<=,>=.
 = and =! are likely to miss and make an inf loop
 youll find that sometimes be off by 1, frequently the solution is just to change between < <= > >=
+
+you can skip elements of the loop declaration
+let i=0
+for(;i<3, i++){
+    console.log(i)
+}
+for(let i=0; i<3){
+console.log(i++)
+}
+let i = 0
+for (; i<3;){
+    console.log(i)
+    i++
+}
+for (;;) {//inf loop
+}
+
+BREAK - a command that will immediately exit you from a loop and advance to te next part of your code
+put breaks in as failsafes if a inf loop is possible */
+//ex 1 - get a number form the user
+// let sum=0
+// while(true){
+//     let value= +prompt("enter a number",'')
+//     if (!value){
+//         break
+//     }
+//     sum+=value
+// }
+// console.log(`sum=`+ sum)
+
+//ex 2 - add num from 1 to a given value with a timeout function
+// let sum=0
+// let now=Date.now()
+// let max = +prompt(`add the numbers from 1 to...`)
+// for (let i=1; i<=max; i++){
+//     sum += i
+//     if (Date.now()>now+2000){
+//         alert(`uh`)
+//         break
+//     }
+// }
+// console.log(sum)
+
+/*continue is like a lighter version of break
+instead of ejecting you out of the loop it skips to the end of the iteration
+example - print odd numbers from 1 to 20*/
+// for(let i=0;i<23;i++){
+//     if(i%2==0){
+//         continue;
+//     }
+//     console.log (i)
+// }
+
