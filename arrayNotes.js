1daa9c8d71358cc85c9a747e509e5370f7ff5ee5
Ries <25RiesC@badinhs.org>
2025-02-18 13:43:00 -0500

    progress

--- arrayNotes.js
+++ arrayNotes.js
@@ -10,4 +10,30 @@ let fakeArray= new Array()
 let fakeArray2= []
 */
 let fruits=["apples", "bananas", "cantalope", "durian", "lychees", ]
+console.log(fruits[3])
+//arrays are 0 indexed - start counting from 0
+fruits[3]="watermelon"
 console.log(fruits)
+//add strawberries
+fruits[5]="strawberries"
+console.log(fruits)
+//count how long an array is
+console.log(fruits.length)
+fruits[fruits.length]= "blueberries"
+console.log(fruits)
+//an array can store elements of any type, including mixing types
+// let exampleArray2=["apple", 35, true, functon() {console.log(`hello`);}, "array"]
+// console.log(exampleArray2[1])
+// console.log(exampleArray2[2])
+// console.log(exampleArray2[3])
+//do math - distance formula
+let obj1=[3,5];
+let obj2=[9,11];
+let dist=Math.sqrt(((obj1[0]-obj2[0])**2)+((obj1[1]-obj2[1])**2));
+console.log(dist)
+let primes=[2, 3, 5, 7, 11, 13, 17];
+let i=0
+while(i<primes.length){
+    console.log(primes[i])
+    i++
+}
