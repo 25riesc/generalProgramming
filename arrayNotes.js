440384514bca2197620341fd66854f02a847f2ae
Ries <25RiesC@badinhs.org>
2025-02-18 13:19:11 -0500

    started arrayNotes

---
+++ arrayNotes.js
@@ -0,0 +1,13 @@
+/*arrays
+in programming an array is a collection of values
+an ordered collection of values
+simpler than objects
+most programming langs treat arrays the same
+format:
+let arrayName=[item1, item2, item3, item4]
+empty:
+let fakeArray= new Array()
+let fakeArray2= []
+*/
+let fruits=["apples", "bananas", "cantalope", "durian", "lychees", ]
+console.log(fruits)
